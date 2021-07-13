b89651c2b4b88051daa8765fc8082339bed6a2aa
[Ewan Breakey s3845382] <s3845382@student.rmit.edu.au>
2021-07-13 12:51:28 +1000

    Add mobile view breakpoints

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import { HashRouter as Router } from 'react-router-dom'
 
 import './App.css'
 
+import RobotImage from '../RobotImage/RobotImage'
 import GameBox from '../GameBox/GameBox'
 
 const App = () => {
   return (
     <Router>
       <div className='app'>
+        <RobotImage isExternal={true} />
         <GameBox />
       </div>
     </Router>
